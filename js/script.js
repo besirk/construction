27c83c67323e5c7b33d03e79c0003f29dcf63bb0
Besir Kurtishi <kurtishi45@gmail.com>
2022-11-17 19:58:28 +0100

    changed the website

--- js/script.js
+++ js/script.js
@@ -1,23 +1,45 @@
 let navbar = document.querySelector(".header .navbar");
 
 let contactInfo = document.querySelector(".contact-info");
+let langMenu = document.querySelector(".lang-menu");
+let langbtn = document.querySelector(".lang-btn");
 
 document.querySelector("#menu-btn").onclick = () => {
   navbar.classList.toggle("active");
+  langbtn.classList.remove("clicked");
+  langMenu.classList.remove("activee");
 };
 
 document.querySelector("#info-btn").onclick = () => {
   contactInfo.classList.add("active");
+  navbar.classList.remove("active");
+  langbtn.classList.remove("clicked");
+  langMenu.classList.remove("activee");
+
+};
+document.querySelector("#info-btnn").onclick = () => {
+  contactInfo.classList.add("active");
+  navbar.classList.remove("active");
+  langbtn.classList.remove("clicked");
+  langMenu.classList.remove("activee");
 };
 
 document.querySelector("#close-contact-info").onclick = () => {
   contactInfo.classList.remove("active");
 };
 
+document.querySelector("#lang-btn").onclick = () => {
+  langbtn.classList.toggle("clicked")
+  langMenu.classList.toggle("activee");
+  navbar.classList.remove("active");
+  contactInfo.classList.remove("active");
+};
+
 window.onscroll = () => {
   navbar.classList.remove("active");
-
   contactInfo.classList.remove("active");
+  langbtn.classList.remove("clicked");
+  langMenu.classList.remove("activee");
 };
 
 var swiper = new Swiper(".home-slider", {
@@ -72,10 +94,11 @@ var swiper = new Swiper(".home-slider", {
 // var swiper = new Swiper(".logo-slider", {
 //    loop:true,
 //    grabCursor:true,
-//    spaceBetween: 20,
+//    autoplay:{delay:800,disableOnInteraction: false},
+//    spaceBetween: 10,
 //    breakpoints: {
 //       450: {
-//          slidesPerView: 2,
+//          slidesPerView: 3,
 //        },
 //       640: {
 //         slidesPerView: 3,
